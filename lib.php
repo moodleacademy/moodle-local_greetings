63a6c03152db28b8e999df57f97cf6ae5d1f2187
Rajneel Totaram <rajneel.totaram@moodle.com>
2023-04-11 14:23:21 +1200

    Add plugin link to navigation

--- lib.php
+++ lib.php
@@ -23,6 +23,19 @@
  */
 
 /**
+ * Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in the navigation tree.
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
+    $frontpage->add(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+    );
+}
+
+/**
  * Get a localised greeting message for a user
  *
  * @param \stdClass $user
