89b0d6f7e4eca91981a6ae51f91c389f37c0d1cd
Rajneel Totaram <rajneel.totaram@moodle.com>
2023-04-16 00:34:55 +1200

    Display greetings messages from the database

--- index.php
+++ index.php
@@ -56,4 +56,21 @@ if (isloggedin()) {
 
 $messageform->display();
 
+$messages = $DB->get_records('local_greetings_messages');
+
+echo $OUTPUT->box_start('card-columns');
+
+foreach ($messages as $m) {
+    echo html_writer::start_tag('div', array('class' => 'card'));
+    echo html_writer::start_tag('div', array('class' => 'card-body'));
+    echo html_writer::tag('p', $m->message, array('class' => 'card-text'));
+    echo html_writer::start_tag('p', array('class' => 'card-text'));
+    echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
+    echo html_writer::end_tag('p');
+    echo html_writer::end_tag('div');
+    echo html_writer::end_tag('div');
+}
+
+echo $OUTPUT->box_end();
+
 echo $OUTPUT->footer();
