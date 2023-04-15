9a3ccf4a9826c2c4b9b55628da8598a4f76bf9c6
Rajneel Totaram <rajneel.totaram@moodle.com>
2023-04-15 22:44:48 +1200

    Create a form

--- lang/en/local_greetings.php
+++ lang/en/local_greetings.php
@@ -32,3 +32,4 @@ $string['greetinguserau'] = 'Hello, {$a}.';
 $string['greetinguseres'] = 'Hola, {$a}.';
 $string['greetinguserfj'] = 'Bula, {$a}.';
 $string['greetingusernz'] = 'Kia Ora, {$a}.';
+$string['yourmessage'] = 'Your message';
