87d3ec7ca71b159979a9e9b8818c441bbb05cd76
Rajneel Totaram <rajneel.totaram@moodle.com>
2023-04-16 22:45:46 +1200

    Only logged in users should see link

--- lib.php
+++ lib.php
@@ -28,11 +28,13 @@
  * @param navigation_node $frontpage Node representing the front page in the navigation tree.
  */
 function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
-    $frontpage->add(
-        get_string('pluginname', 'local_greetings'),
-        new moodle_url('/local/greetings/index.php'),
-        navigation_node::TYPE_CUSTOM,
-    );
+    if (isloggedin() && !isguestuser()) {
+        $frontpage->add(
+            get_string('pluginname', 'local_greetings'),
+            new moodle_url('/local/greetings/index.php'),
+            navigation_node::TYPE_CUSTOM,
+        );
+    }
 }
 
 /**
